f2120988a40075e3b4a7f4594732e1aa98a8e8e6
juanrebo <juanrebo@outlook.es>
2018-07-24 19:33:18 +0200

    Engadidos DAO e DAOImp de elementos de clasificación

--- src/com/setas/util/DAOFactory.java
+++ src/com/setas/util/DAOFactory.java
@@ -3,9 +3,13 @@ package com.setas.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.setas.dao.DivisionDAO;
+import com.setas.dao.FamiliaDAO;
 import com.setas.dao.RolDAO;
 import com.setas.dao.SetaDAO;
 import com.setas.dao.UsuarioDAO;
+import com.setas.daoimp.DivisionDAOImp;
+import com.setas.daoimp.FamiliaDAOImp;
 import com.setas.daoimp.RolDAOImp;
 import com.setas.daoimp.SetaDAOImp;
 import com.setas.daoimp.UsuarioDAOImp;
@@ -32,5 +36,13 @@ public class DAOFactory {
 	
 	public UsuarioDAO getUsuario() {
 		return new UsuarioDAOImp(sf);
+	}
+	
+	public DivisionDAO getDivision() {
+		return new DivisionDAOImp(sf);
+	}
+	
+	public FamiliaDAO getFamilia() {
+		return new FamiliaDAOImp(sf);
 	}
 }
